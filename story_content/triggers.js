510bf54c8f6f0e723fc918bead0683b6803fefbe
KausarMohammad <55966879+KausarMohammad@users.noreply.github.com>
2024-07-05 11:36:38 -0400

    Add files via upload

--- story_content/triggers.js
+++ story_content/triggers.js
@@ -2,6 +2,36 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
+      case "5iq255RBFoL":
+        Script1();
+        break;
+      case "6XxJcX7E0uI":
+        Script2();
+        break;
+      case "5b4GQdIWBWV":
+        Script3();
+        break;
+      case "6J5SL7BTHAl":
+        Script4();
+        break;
+      case "5jHCGM5wDI2":
+        Script5();
+        break;
+      case "5YM8FB5b67F":
+        Script6();
+        break;
+      case "6973UFDEtZ3":
+        Script7();
+        break;
+      case "5zF5U9HubBV":
+        Script8();
+        break;
+      case "6P1hx93yo6V":
+        Script9();
+        break;
+      case "5kXzRkNbs3l":
+        Script10();
+        break;
   }
 }
 
--- story_content/user.js
+++ story_content/user.js
@@ -5,4 +5,204 @@ var object = player.object;
 var addToTimeline = player.addToTimeline;
 var setVar = player.SetVar;
 var getVar = player.GetVar;
+window.Script1 = function()
+{
+  let player = GetPlayer();
+let Value = player.GetVar("Progress"); 
+ 
+function updateProgressBar(newValue) {
+  // update the global 'value'
+  value = newValue;
+ 
+  // get the progress bar and text elements
+  let progressbar = document.getElementById('progressbar');
+  let progressText = document.querySelector('#progressContainer div:last-child');
+ 
+  // update the progress bar width and text
+  progressbar.style.width = value + '%';
+  progressText.innerHTML = value + '% Completed';
+}
+updateProgressBar(Value);
+}
+
+window.Script2 = function()
+{
+  let player = GetPlayer();
+let Value = player.GetVar("Progress"); 
+ 
+function updateProgressBar(newValue) {
+  // update the global 'value'
+  value = newValue;
+ 
+  // get the progress bar and text elements
+  let progressbar = document.getElementById('progressbar');
+  let progressText = document.querySelector('#progressContainer div:last-child');
+ 
+  // update the progress bar width and text
+  progressbar.style.width = value + '%';
+  progressText.innerHTML = value + '% Completed';
+}
+updateProgressBar(Value);
+}
+
+window.Script3 = function()
+{
+  let player = GetPlayer();
+let Value = player.GetVar("Progress"); 
+ 
+function updateProgressBar(newValue) {
+  // update the global 'value'
+  value = newValue;
+ 
+  // get the progress bar and text elements
+  let progressbar = document.getElementById('progressbar');
+  let progressText = document.querySelector('#progressContainer div:last-child');
+ 
+  // update the progress bar width and text
+  progressbar.style.width = value + '%';
+  progressText.innerHTML = value + '% Completed';
+}
+updateProgressBar(Value);
+}
+
+window.Script4 = function()
+{
+  let player = GetPlayer();
+let Value = player.GetVar("Progress"); 
+ 
+function updateProgressBar(newValue) {
+  // update the global 'value'
+  value = newValue;
+ 
+  // get the progress bar and text elements
+  let progressbar = document.getElementById('progressbar');
+  let progressText = document.querySelector('#progressContainer div:last-child');
+ 
+  // update the progress bar width and text
+  progressbar.style.width = value + '%';
+  progressText.innerHTML = value + '% Completed';
+}
+updateProgressBar(Value);
+}
+
+window.Script5 = function()
+{
+  let player = GetPlayer();
+let Value = player.GetVar("Progress"); 
+ 
+function updateProgressBar(newValue) {
+  // update the global 'value'
+  value = newValue;
+ 
+  // get the progress bar and text elements
+  let progressbar = document.getElementById('progressbar');
+  let progressText = document.querySelector('#progressContainer div:last-child');
+ 
+  // update the progress bar width and text
+  progressbar.style.width = value + '%';
+  progressText.innerHTML = value + '% Completed';
+}
+updateProgressBar(Value);
+}
+
+window.Script6 = function()
+{
+  let player = GetPlayer();
+let Value = player.GetVar("Progress"); 
+ 
+function updateProgressBar(newValue) {
+  // update the global 'value'
+  value = newValue;
+ 
+  // get the progress bar and text elements
+  let progressbar = document.getElementById('progressbar');
+  let progressText = document.querySelector('#progressContainer div:last-child');
+ 
+  // update the progress bar width and text
+  progressbar.style.width = value + '%';
+  progressText.innerHTML = value + '% Completed';
+}
+updateProgressBar(Value);
+}
+
+window.Script7 = function()
+{
+  let player = GetPlayer();
+let Value = player.GetVar("Progress"); 
+ 
+function updateProgressBar(newValue) {
+  // update the global 'value'
+  value = newValue;
+ 
+  // get the progress bar and text elements
+  let progressbar = document.getElementById('progressbar');
+  let progressText = document.querySelector('#progressContainer div:last-child');
+ 
+  // update the progress bar width and text
+  progressbar.style.width = value + '%';
+  progressText.innerHTML = value + '% Completed';
+}
+updateProgressBar(Value);
+}
+
+window.Script8 = function()
+{
+  let player = GetPlayer();
+let Value = player.GetVar("Progress"); 
+ 
+function updateProgressBar(newValue) {
+  // update the global 'value'
+  value = newValue;
+ 
+  // get the progress bar and text elements
+  let progressbar = document.getElementById('progressbar');
+  let progressText = document.querySelector('#progressContainer div:last-child');
+ 
+  // update the progress bar width and text
+  progressbar.style.width = value + '%';
+  progressText.innerHTML = value + '% Completed';
+}
+updateProgressBar(Value);
+}
+
+window.Script9 = function()
+{
+  let player = GetPlayer();
+let Value = player.GetVar("Progress"); 
+ 
+function updateProgressBar(newValue) {
+  // update the global 'value'
+  value = newValue;
+ 
+  // get the progress bar and text elements
+  let progressbar = document.getElementById('progressbar');
+  let progressText = document.querySelector('#progressContainer div:last-child');
+ 
+  // update the progress bar width and text
+  progressbar.style.width = value + '%';
+  progressText.innerHTML = value + '% Completed';
+}
+updateProgressBar(Value);
+}
+
+window.Script10 = function()
+{
+  let player = GetPlayer();
+let Value = player.GetVar("Progress"); 
+ 
+function updateProgressBar(newValue) {
+  // update the global 'value'
+  value = newValue;
+ 
+  // get the progress bar and text elements
+  let progressbar = document.getElementById('progressbar');
+  let progressText = document.querySelector('#progressContainer div:last-child');
+ 
+  // update the progress bar width and text
+  progressbar.style.width = value + '%';
+  progressText.innerHTML = value + '% Completed';
+}
+updateProgressBar(Value);
+}
+
 };
